3cd00674bf66f18783a883a8e6d2b960f40dd965
joshua1923 <joshua_4991@hotmail.co.uk>
2020-08-05 15:20:31 +0100

    changed endpoint

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ var morgan = require('morgan')
 
 morgan.token('data', (req, res) => {return JSON.stringify(req.body)})
 
-app.use(express.static(path.join(__dirname, 'client/build')))
+app.use(express.static(path.join(__dirname, '/Client/build')))
 app.use(express.json())
 app.use(cors())
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :data'))
